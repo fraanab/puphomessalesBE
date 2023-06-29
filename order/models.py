6f46f714af16e963781a11578882fd6e5d2b7328
sentey <91223447+fraanab@users.noreply.github.com>
2023-06-29 17:55:44 -0300

    reset

--- order/models.py
+++ order/models.py
@@ -6,7 +6,7 @@ from product.models import Product
 
 
 class Order(models.Model):
-    orderid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    orderid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     total_ammount = models.IntegerField(default=0)                          # set this to the total amount
     paid = models.BooleanField(default=False)                               # set this to true if paid
 
@@ -33,4 +33,4 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
-        return self.id
+        return f'id {self.id} | order pk {self.order} | order id {self.orderid}'
