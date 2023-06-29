23f1d289972b36e55332a5637ad347b47fa4901d
sentey <91223447+fraanab@users.noreply.github.com>
2023-06-28 22:41:51 -0300

    pgsql

--- backend/settings.py
+++ backend/settings.py
@@ -90,9 +90,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': os.getenv('DB_NAME'),
+        'USERNAME': os.getenv('DB_USERNAME'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOSTNAME': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
